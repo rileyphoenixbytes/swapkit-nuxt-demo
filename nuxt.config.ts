399435ac88402f6ca98f6b57671f87fe00367f28
Revo <rileyphoenixbytes@gmail.com>
2024-04-30 17:40:46 +0200

    chore: add vite config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,62 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  vite: {
+    build: {
+      target: 'esnext',
+      rollupOptions: {
+        output: {
+          manualChunks: (id) => {
+            // isolate randomfill as a separate chunk
+            if (id.includes('node_modules/randomfill/browser.js')) {
+              return 'randomfill'
+            }
+          },
+        },
+      },
+    },
+    define: {
+      global: 'globalThis',
+    },
+    plugins: [
+      {
+        // work around for `exports is not defined` error within the crypto-browserify > randomfill dep
+        name: 'crypto-randomfill-patch',
+
+        apply: 'build',
+        enforce: 'pre',
+
+        renderChunk(code, chunk) {
+          if (!/randomfill.*\.js$/.test(chunk.fileName)) {
+            return null
+          }
+
+          const modifiedCode = code
+            .split('\n')
+            .map((line) => {
+              if (line.trim() === 'exports.randomFill = randomFill;') {
+                return 'var exports = {}; exports.randomFill = randomFill;'
+              }
+
+              return line
+            })
+            .join('\n')
+
+          return { code: modifiedCode, map: null }
+        },
+      },
+    ],
+
+    resolve: {
+      alias: {
+        // required to polyfill node crypto module
+        crypto: 'crypto-browserify',
+        stream: 'stream-browserify',
+
+        buffer: 'unenv/runtime/node/buffer/index',
+        'node:buffer': 'unenv/runtime/node/buffer/index',
+      },
+    },
+  },
+
 })
