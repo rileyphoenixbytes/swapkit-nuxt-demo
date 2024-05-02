65bb65ba0a1e3935aeb70174106c6d49c7a599e6
Mateja Petrovic <mateja176@gmail.com>
2024-05-02 12:06:15 +0300

    fix(swapkit): replaces @ts-expect-error with the proper type declaration

--- swapkit.ts
+++ swapkit.ts
@@ -1,21 +1,7 @@
-import { ChainflipPlugin } from "@swapkit/chainflip";
-import {
-  AssetValue,
-  SwapKitApi,
-  type ConnectWalletParams,
-} from "@swapkit/core";
-import { SwapKit, Chain } from "@swapkit/core";
-import { MayachainPlugin, ThorchainPlugin } from "@swapkit/thorchain";
-import { coinbaseWallet } from "@swapkit/wallet-coinbase";
-import { evmWallet } from "@swapkit/wallet-evm-extensions";
-import { keepkeyWallet } from "@swapkit/wallet-keepkey";
-import { keplrWallet } from "@swapkit/wallet-keplr";
-import { keystoreWallet } from "@swapkit/wallet-keystore";
-import { ledgerWallet } from "@swapkit/wallet-ledger";
-import { okxWallet } from "@swapkit/wallet-okx";
-import { trezorWallet } from "@swapkit/wallet-trezor";
-import { walletconnectWallet } from "@swapkit/wallet-wc";
-import { xdefiWallet } from "@swapkit/wallet-xdefi";
+import { AssetValue, SwapKitApi } from '@swapkit/core';
+import { Chain, SwapKit } from '@swapkit/core';
+import { MayachainPlugin, ThorchainPlugin } from '@swapkit/thorchain';
+import { xdefiWallet } from '@swapkit/wallet-xdefi';
 
 const wallets = {
   ...xdefiWallet,
@@ -30,14 +16,14 @@ type Client = ReturnType<typeof SwapKit<typeof plugins, typeof wallets>>;
 
 let sdkClient: Client;
 
-export const getSwapKitClient = async () => {
+export const getSwapKitClient = () => {
   if (sdkClient) return sdkClient;
 
   const core = SwapKit({
     apis: {},
     rpcUrls: {},
     stagenet: false,
-    config: { ethplorerApiKey: "freekey" },
+    config: { ethplorerApiKey: 'freekey' },
     wallets,
     plugins,
   });
@@ -47,19 +33,32 @@ export const getSwapKitClient = async () => {
   return sdkClient;
 };
 
-const connectChains = [Chain.Ethereum, Chain.Bitcoin, Chain.THORChain];
+const connectChains = [
+  Chain.Ethereum,
+  Chain.Bitcoin,
+  Chain.THORChain,
+] satisfies Parameters<typeof sdkClient.connectXDEFI>[0];
 
 export const connectWallet = async () => {
   await AssetValue.loadStaticAssets();
-  await getSwapKitClient();
-  // @ts-expect-error
   return sdkClient.connectXDEFI(connectChains);
 };
 
 export const fetchWalletBalances = async () => {
-  const wallets = await Promise.all(
-    connectChains.map((chain) => sdkClient.getWalletWithBalance(chain))
-  );
+  const wallets = (
+    await Promise.allSettled(
+      connectChains.map((chain) => sdkClient.getWalletWithBalance(chain)),
+    )
+  ).flatMap((result, i) => {
+    if (result.status === 'fulfilled') {
+      return result.value;
+    } else {
+      console.error(
+        `Failed to get ${connectChains[i]} chain wallet balance: ${result.reason}`,
+      );
+      return [];
+    }
+  });
 
   console.log(wallets);
   // [{ balance: AssetAmount[]; address: string; walletType: WalletOption }]
@@ -77,17 +76,17 @@ export const swap = async () => {
   const senderAddress = sdkClient.getWallet(Chain.Ethereum).address;
   const recipientAddress = sdkClient.getWallet(Chain.THORChain).address;
   const { routes } = await SwapKitApi.getSwapQuote({
-    sellAsset: "ETH.ETH",
-    sellAmount: "0.01",
-    buyAsset: "THOR.RUNE",
+    sellAsset: 'ETH.ETH',
+    sellAmount: '0.01',
+    buyAsset: 'THOR.RUNE',
     senderAddress,
     recipientAddress,
-    slippage: "3",
+    slippage: '3',
   });
   console.log(routes);
 
   return sdkClient.swap({
-    pluginName: "thorchain",
+    pluginName: 'thorchain',
     recipient: recipientAddress,
     route: routes[0],
   });
